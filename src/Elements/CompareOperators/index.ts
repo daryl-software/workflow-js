4367833d906ebd4aab770961bd7febc0bb06bbf5
Anthony Loup <anthony.loup@easyflirt.com>
2022-02-22 11:47:45 +0100

    add operators

--- src/Elements/CompareOperators/index.ts
+++ src/Elements/CompareOperators/index.ts
@@ -5,5 +5,8 @@ import GreaterOrEqual from './GreaterOrEqual';
 import Less from './Less';
 import LessOrEqual from './LessOrEqual';
 import Not from './Not';
+import In from './In';
+import NotIn from './NotIn';
+import NotEqual from './NotEqual';
 
-export {CompareOperator, Equal, Greater, GreaterOrEqual, Less, LessOrEqual, Not};
+export {CompareOperator, Equal, Greater, GreaterOrEqual, Less, LessOrEqual, Not, NotEqual, In, NotIn};
